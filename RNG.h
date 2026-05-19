979316ee6d8d9a6fcae3ffc64c856172e148e709
joeymatella2 <111457339+joeymatella2@users.noreply.github.com>
2026-05-18 22:47:10 -0700

    Refactor header comments in RNG.h
    Updated header comments and authors in RNG.h.

--- RNG.h
+++ RNG.h
@@ -1,32 +1,23 @@
-/*
- *******************************************************************************
- * @file           : RNG.h
- * @brief          : X
- * project         : EE 329 S'26 AX
- * authors         : joeym
- * version         : 0.1
- * date            : May 17, 2026
- * compiler        : STM32CubeIDE v.1.19.0 Build: 14980_20230301_1550 (UTC)
- * target          : NUCLEO-L4A6ZG
- * clocks          : 4 MHz MSI to AHB2
- * @attention      : (c) 2026 STMicroelectronics.  All rights reserved.
- *******************************************************************************
- * Description: X
- *
- *******************************************************************************
- * GPIO Wiring
- * |   Component    | GPIO Identifier | Connector Location | Config
- *-----------------------------------------------------------------------------
- * | LCD - DB4 - 11 | PC0             | CN9-3              | OUT
- *******************************************************************************
- * Version History
- *  Ver.|   Date   |  Description
- *  ---------------------------------------------------------------------------
- *      |          | 
- *******************************************************************************
- *
- * Header format adapted from [Code Appendix by Kevin Vo] pg 5
- */
+/**
+  ******************************************************************************
+  * @file           : RNG.h
+  * @brief          : Header for RNG.h file.
+  * project         : EE 329 S'26 A9
+  * authors         : Joseph Matella, Nathan Cook, Tyler Ragasa
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2026 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+
 
 
 #ifndef INC_RNG_H_
